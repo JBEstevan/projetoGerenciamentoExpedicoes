29c27d3e34c2697261b681e4473ff7dc2024f691
JBEstevan <jbe.estevan@gmail.com>
2024-10-28 20:21:46 -0300

    feat: add packages, app

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+"dotenv/config";
+import explorer_router from './routers/explorer_router.js';
+import species_router from './routers/species_router.js';
+import expedition_router from './routers/expedition_router.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/explorer', explorer_router);
+app.use('/species', species_router);
+app.use('/expedition', expedition_router);
+
+app.listen(3001, () => console.log('Server is running on port 3001'));
