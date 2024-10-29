bea866dabb5bf013fe8085659baf974eb13df3f0
JBEstevan <jbe.estevan@gmail.com>
2024-10-28 21:13:09 -0300

    feat: add configuração do banco

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import express from 'express';
 "dotenv/config";
 import explorer_router from './routers/explorer_router.js';
-import species_router from './routers/species_router.js';
-import expedition_router from './routers/expedition_router.js';
+/* import species_router from './routers/species_router.js';
+import expedition_router from './routers/expedition_router.js'; */
+
+import db from './config/dbConfig.js';
 
 const app = express();
 
+
+db();
 app.use(express.json());
 
 app.use('/explorer', explorer_router);
-app.use('/species', species_router);
-app.use('/expedition', expedition_router);
+/* app.use('/species', species_router);
+app.use('/expedition', expedition_router); */
 
 app.listen(3001, () => console.log('Server is running on port 3001'));
